bbabc85cfb5628ef3e794c2514d7f4962650a1f7
Ki Ki  Bush <donyabush@gmail.com>
2018-08-08 23:16:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
-// Code your solution in this file.
 function toLower(driver) {
   return driver.toLowerCase();
 }
-function lowerCaseDrivers(drivers) {
+
+function lowerCaseDrivers(drivers){
   return drivers.map(toLower)
 }
 
-  function nameToAttributes(drivers) {
+function nameToAttributes(drivers){
   return drivers.map(function (driver) {
     splitName = driver.split(' ')
+    console.log(splitName)
     return {firstName: splitName[0], lastName: splitName[1]}
   })
-  
- 
+
 }
 
-  function attributesToPhrase(drivers) {
-  return drivers.map(function(driver) {
-  return driver.name + 'is from' + driver.hometown
+function attributesToPhrase(drivers) {
+  return drivers.map(function(driver){
+    return driver.name + ' is from ' + driver.hometown
   })
-  }
-  
-  
-const drivers = [
-        { name: 'Bobby',   hometown: 'Pittsburgh'  },
-        { name: 'Sammy',   hometown: 'New York'    },
-        { name: 'Sally',   hometown: 'Cleveland'   },
-        { name: 'Annette', hometown: 'Los Angeles' },
-        { name: 'Bobby',   hometown: 'Tampa Bay'   }
-      ];
+}
